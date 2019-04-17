dc32ef3439323801c51acf4c1f191b5b5720ce0a
Natalia PN <npelegero@gmail.com>
2019-04-17 12:46:23 +0200

    Add docs folder

---
+++ docs/precache-manifest.d6e3c31de6fed2b735c855738ff4166f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "6c1e86a617d147a83aa1",
+    "url": "./static/js/main.d9e3eab3.chunk.js"
+  },
+  {
+    "revision": "00061effefbb1c10b3f5",
+    "url": "./static/js/2.eba9f466.chunk.js"
+  },
+  {
+    "revision": "6c1e86a617d147a83aa1",
+    "url": "./static/css/main.7e895288.chunk.css"
+  },
+  {
+    "revision": "92b764a5e9c3e35057f388a95ef73e76",
+    "url": "./index.html"
+  }
+];
